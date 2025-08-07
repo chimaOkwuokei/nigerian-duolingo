61dcc98ee15fc7ec57a4d112dff9f6e591eeb3b9
Chima Okwuokei <chima.okwuokei@pau.edu.ng>
2025-08-07 12:22:37 +0100

    chore: protection of sensitive information

--- naija-duo-frontend/src/services/firebase.ts
+++ naija-duo-frontend/src/services/firebase.ts
@@ -3,21 +3,21 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
- const API_KEY = import.meta.env.API_KEY;
- const AUTH_DOMAIN = import.meta.env.AUTH_DOMAIN;
- const PROJECT_ID = import.meta.env.PROJECT_ID;
- const STORAGE_BUCKET = import.meta.env.STORAGE_BUCKET;
- const MESSAGING_SENDER_ID = import.meta.env.MESSAGING_SENDER_ID;
- const APP_ID = import.meta.env.APP_ID;
+const API_KEY = import.meta.env.VITE_API_KEY;
+const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
+const PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
+const STORAGE_BUCKET = import.meta.env.VITE_STORAGE_BUCKET;
+const MESSAGING_SENDER_ID = import.meta.env.VITE_MESSAGING_SENDER_ID;
+const APP_ID = import.meta.env.VITE_APP_ID;
 
- // Your web app's Firebase configuration
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: API_KEY,
   authDomain: AUTH_DOMAIN,
   projectId: PROJECT_ID,
-  storageBucket:STORAGE_BUCKET,
+  storageBucket: STORAGE_BUCKET,
   messagingSenderId: MESSAGING_SENDER_ID,
-  appId:APP_ID
+  appId: APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
